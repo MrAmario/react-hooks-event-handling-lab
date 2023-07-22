8ac493991a8504757754a53ea004ddd773613b11
Maxwell <maxwell.nganga@student.moringaschool.com>
2023-07-23 02:33:06 +0300

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+
+function handleBlur(event){
+    console.log('Hey! Eyes on me!')
+}
+
+function handleFocus(event){
+    console.log('Good!')
+}
+
+function EyesOnMe(){
+   return( <button onBlur={handleBlur} onFocus={handleFocus} >Eyes on me</button>
+)}
+
+export default EyesOnMe;
